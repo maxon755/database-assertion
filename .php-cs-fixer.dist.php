9ecdbdff171bfa91f4eb99fbbae393653882ca35
Maksym Haiduk <maksym.haiduk@gmail.com>
2022-09-09 17:31:13 +0300

    Add dependencies

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,19 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude(['var', 'vendor'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'phpdoc_no_alias_tag' => ['replacements' => ['type' => 'var', 'link' => 'see']],
+        'phpdoc_to_comment' => false,
+        'single_line_comment_style' => false,
+    ])
+    ->setFinder($finder)
+;
