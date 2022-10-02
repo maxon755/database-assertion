3686f11323768bc29780d874f1e920a431e6c9db
Maksym Haiduk <maksym.haiduk@gmail.com>
2022-10-02 18:12:20 +0300

    Create WhereCondition

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace Maxon755\DatabaseAssertion;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\QueryBuilder as DoctrineQueryBuilder;
+use Maxon755\DatabaseAssertion\Condition\WhereCondition;
 
 class QueryBuilder
 {
     /**
      * @param array<mixed> $parameters
      */
-    public static function buildCountQuery(Connection $connection, string $table, array $parameters): \Doctrine\DBAL\Query\QueryBuilder
+    public static function buildCountQuery(Connection $connection, string $table, array $parameters): DoctrineQueryBuilder
     {
         $queryBuilder = $connection->createQueryBuilder();
 
@@ -20,12 +22,29 @@ class QueryBuilder
             ->from($table)
         ;
 
-        foreach ($parameters as $name => $value) {
-            $queryBuilder->andWhere("{$name} = :{$name}");
-
-            $queryBuilder->setParameter($name, $value);
+        foreach ($parameters as $key => $value) {
+            self::buildWhereCondition($queryBuilder, $key, $value);
         }
 
         return $queryBuilder;
+    }
+
+    /**
+     * @param mixed      $value
+     * @param int|string $key
+     */
+    private static function buildWhereCondition(DoctrineQueryBuilder $queryBuilder, $key, $value): void
+    {
+        if ($value instanceof WhereCondition) {
+            $name = $value->getColumn();
+            $operation = $value->getOperation();
+            $value = $value->getValue();
+        } else {
+            $name = $key;
+            $operation = '=';
+        }
+
+        $queryBuilder->andWhere("{$name} {$operation} :{$name}");
+        $queryBuilder->setParameter($name, $value);
     }
 }
